d22f6af92ce05920c599cd44fca4d7ac6a6d9674
C. Titus Brown <titus@idyll.org>
2017-03-02 17:18:22 -0800

    scripts

---
+++ lab7/yeast.salmon.R
@@ -0,0 +1,44 @@
+library("edgeR")
+
+files <- c(
+"ERR458493.fastq.gz.quant.counts",
+"ERR458494.fastq.gz.quant.counts",
+"ERR458500.fastq.gz.quant.counts",
+"ERR458501.fastq.gz.quant.counts"
+)
+
+labels=c("A", "B", "C", "D")
+
+data <- readDGE(files)
+
+print(data)
+
+###
+
+group <- c(rep("I",2), rep("II",2))
+
+dge = DGEList(counts=data, group=group)
+dge <- estimateCommonDisp(dge)
+dge <- estimateTagwiseDisp(dge)
+
+# make an MA-plot
+
+et <- exactTest(dge, pair=c("I", "II"))
+etp <- topTags(et, n=100000)
+etp$table$logFC = -etp$table$logFC
+pdf("yeast-edgeR-MA-plot.pdf")
+plot(
+  etp$table$logCPM,
+  etp$table$logFC,
+  xlim=c(-3, 20), ylim=c(-12, 12), pch=20, cex=.3,
+  col = ifelse( etp$table$FDR < .2, "red", "black" ) )
+dev.off()
+
+# plot MDS
+pdf("yeast-edgeR-MDS.pdf")
+plotMDS(dge, labels=labels)
+dev.off()
+
+# output CSV for 0-6 hr
+write.csv(etp$table, "yeast-edgeR.csv")
+

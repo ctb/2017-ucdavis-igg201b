d5ae58a7e51fae15d012d6031e3a07b96dcb2ae4
Harriet Alexander <harriet.xander@gmail.com>
2017-03-17 08:12:55 -0700

    Update install-pathview-clusterProfiler.R

--- lab9/install-pathview-clusterProfiler.R
+++ lab9/install-pathview-clusterProfiler.R
@@ -1,5 +1,4 @@
 source("https://bioconductor.org/biocLite.R")
-biocLite('edgeR', suppressUpdates=TRUE, siteRepos=c('http://cran.us.r-project.org'))
 biocLite('pathview', suppressUpdates=TRUE, siteRepos=c('http://cran.us.r-project.org'))
 biocLite('KEGGREST', suppressUpdates=TRUE, siteRepos=c('http://cran.us.r-project.org'))
 biocLite("clusterProfiler", suppressUpdates=TRUE, siteRepos=c('http://cran.us.r-project.org'))
